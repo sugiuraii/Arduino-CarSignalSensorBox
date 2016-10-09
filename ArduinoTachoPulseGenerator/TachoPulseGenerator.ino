4a29d65f901894bfe137f2553e64a841785df52b
Sugiura K Nigo <sugiura.k.nigo@gmail.com>
2016-10-09 14:21:15 +0000

    Initial version
    git-svn-id: file:///c/Users/kuniaki/Repository/ArduinoPulseSensorGeneratorReader/trunk@2 70a5437a-bf4d-a74f-a45c-9c4b94b3e3d7

---
+++ ArduinoTachoPulseGenerator/TachoPulseGenerator.ino
@@ -0,0 +1,133 @@
+//Define number of pulse in TACHO and SPEED
+#define PULSE_PER_TACHO_REV 2
+#define PULSE_PER_SPEED_REV 4
+
+//Define serial statusout baudrate
+#define Serial_Baudrate 9600
+
+//Define PIN
+#define speedPin 2
+#define tachoPin 3
+#define speedAnalogVolumePin 0
+#define tachoAnalogVolumePin 1
+
+//Define Analog read interval in microsecond
+#define AnalogReadInterval 1000000UL
+
+int speed = 0;
+int tacho = 0;
+unsigned long speedPulseInterval = 0;
+unsigned long tachoPulseInterval = 0;
+
+//Timer counter for pulse
+unsigned long speedPulseBeforeTime = 0;
+unsigned long tachoPulseBeforeTime = 0;
+unsigned long analogReadBeforeTime = 0;
+int speedPinState = LOW;
+int tachoPinState = LOW;
+
+//Read analog input and set speed/tacho
+void analogReadAndSetSpeedTacho()
+{
+  //Set speed
+  speed = map(analogRead(speedAnalogVolumePin), 0, 1023, 1, 300);
+  //speedPulseInterval = 3600/(637*speed*PULSE_PER_SPEED_REV) * 1E6;
+  speedPulseInterval = 5651491UL/(speed*PULSE_PER_SPEED_REV);
+  speedPulseInterval /= 2;
+  
+  //Set tacho
+  tacho = map(analogRead(tachoAnalogVolumePin), 0, 1023, 1, 10000);
+  //tachoPulseInterval = 60/(PULSE_PER_TACHO_REV*tacho) * 1E6;
+  tachoPulseInterval = 60000000UL/(PULSE_PER_TACHO_REV*tacho);
+  
+  tachoPulseInterval /= 2; 
+}
+
+//Out current tacho/speed to serialPort
+void serialOut(int _speed, int _tacho)
+{
+  Serial.print("Speed:");
+  Serial.print(_speed);
+  Serial.print(" Tacho:");
+  Serial.println(_tacho);
+}
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize the digital pin as an output.
+  pinMode(speedPin, OUTPUT);
+  pinMode(tachoPin, OUTPUT);
+  
+  Serial.begin(Serial_Baudrate);
+  
+  analogReadAndSetSpeedTacho();
+  serialOut(speed, tacho);
+  digitalWrite(speedPin, speedPinState);
+  digitalWrite(tachoPin, tachoPinState);
+
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  unsigned long nowTime;
+  nowTime = micros();
+  
+  unsigned long speedPulseElapsedTime;
+  unsigned long tachoPulseElapsedTime;
+  unsigned long analogReadElapsedTime;
+  
+  //AnalogRead and set pulseInterval
+  if(nowTime > analogReadBeforeTime )
+    analogReadElapsedTime = nowTime - analogReadBeforeTime;
+  else
+    analogReadElapsedTime = nowTime + 4294967295UL - analogReadBeforeTime;
+  if(analogReadElapsedTime > AnalogReadInterval) 
+  {
+    analogReadAndSetSpeedTacho();
+    serialOut(speed, tacho);
+    analogReadBeforeTime = nowTime;
+  }
+  
+  //Set speed pin state
+  if(nowTime > speedPulseBeforeTime )
+    speedPulseElapsedTime = nowTime - speedPulseBeforeTime;
+  else
+    speedPulseElapsedTime = nowTime + 4294967295UL - speedPulseBeforeTime;
+  
+  if(speedPulseElapsedTime > speedPulseInterval) 
+  {
+    if(speedPinState == LOW)
+    {
+      speedPinState = HIGH;
+      digitalWrite(speedPin, speedPinState);
+    }
+    else
+    {
+      speedPinState = LOW;
+      digitalWrite(speedPin, speedPinState);
+    }
+    speedPulseBeforeTime = nowTime;
+  }
+  
+  //Set tacho pin state
+  if(nowTime > tachoPulseBeforeTime )
+    tachoPulseElapsedTime = nowTime - tachoPulseBeforeTime;
+  else
+    tachoPulseElapsedTime = nowTime + 4294967295UL - tachoPulseBeforeTime;
+  
+  if(tachoPulseElapsedTime > tachoPulseInterval) 
+  {
+    if(tachoPinState == LOW)
+    {
+      tachoPinState = HIGH;
+      digitalWrite(tachoPin, tachoPinState);
+    }
+    else
+    {
+      tachoPinState = LOW;
+      digitalWrite(tachoPin, tachoPinState);
+    }
+    tachoPulseBeforeTime = nowTime;
+  }
+}
+
---
+++ ArduinoTachoPulseReader/TachoPulseReader.ino
@@ -0,0 +1,114 @@
+//communication interval in microsecond.
+#define COMM_INTERVAL 16000
+//Speed,Tacho pulse reset time (in microsecond)
+#define SPEED_PULSE_EXPIRE_TIME 500000
+#define TACHO_PULSE_EXPIRE_TIME 500000
+
+//AnalogRead Oversampling count (1:No oversample)
+#define OVERSAMPLE_COUNT 4
+
+int speedPin = 0;
+volatile unsigned long speedPulseBeforeTime=0;
+volatile unsigned long speedPulseElapsedTime=0;
+
+int tachoPin = 1;
+volatile unsigned long tachoPulseBeforeTime=0;
+volatile unsigned long tachoPulseElapsedTime=0;
+
+int analogChannel=0;
+int analogReadVal[6];
+
+unsigned long commBeforeTime = 0;
+
+// the setup routine runs once when you press reset:
+void setup() {
+  pinMode(speedPin, INPUT_PULLUP);
+  pinMode(tachoPin, INPUT_PULLUP);
+  
+  attachInterrupt(speedPin, interruptSpeedPulse, RISING);
+  attachInterrupt(tachoPin, interruptTachoPulse, RISING);
+
+  //SerialPort setting
+  Serial.begin(38400);  
+}
+
+void interruptSpeedPulse()
+{
+  unsigned long nowTime;
+  nowTime = micros();
+  if(nowTime < speedPulseBeforeTime) //Case of micors() overflow
+    speedPulseElapsedTime = nowTime + 4294967295UL - speedPulseBeforeTime;
+  else
+    speedPulseElapsedTime = nowTime - speedPulseBeforeTime;
+    
+  speedPulseBeforeTime = nowTime;
+}
+
+void interruptTachoPulse()
+{
+  unsigned long nowTime;
+  nowTime = micros();
+  if(nowTime < tachoPulseBeforeTime) //Case of micors() overflow
+  tachoPulseElapsedTime = nowTime + 4294967295UL - tachoPulseBeforeTime;
+  else
+    tachoPulseElapsedTime = nowTime - tachoPulseBeforeTime;
+  
+  tachoPulseBeforeTime = nowTime;
+}
+
+
+// the loop routine runs over and over again forever:
+void loop() {
+  int i,j;  
+  for(i = 0; i <=5; i++)
+  {
+    analogReadVal[i] = 0;
+    for(j = 0; j < OVERSAMPLE_COUNT; j++)
+      analogReadVal[i] += analogRead(i);
+  }
+  
+  unsigned long nowTime;
+  unsigned long commElapsedTime;
+  nowTime = micros();
+  if(nowTime < commBeforeTime)
+    commElapsedTime = nowTime + 4294967295UL - commBeforeTime;
+  else
+    commElapsedTime = nowTime - commBeforeTime;
+
+  //Send serial
+  if(commElapsedTime > COMM_INTERVAL)
+  {
+    //Overwrite elapsed time if no pulse is detected in 1sec
+    if((nowTime - speedPulseBeforeTime) > SPEED_PULSE_EXPIRE_TIME)
+      speedPulseElapsedTime = 0;
+    if((nowTime - tachoPulseBeforeTime) > TACHO_PULSE_EXPIRE_TIME)
+      tachoPulseElapsedTime = 0;
+    
+    send();
+    commBeforeTime = nowTime;
+  }
+  
+}
+
+void send(){
+  int i;
+  unsigned int speedPulseCycle=0;
+  unsigned int tachoPulseCycle=0;
+  speedPulseCycle = speedPulseElapsedTime;
+  tachoPulseCycle = tachoPulseElapsedTime;
+
+
+  //Send
+  Serial.print("SP");
+  Serial.print(speedPulseCycle);
+  Serial.print(" TA");
+  Serial.print(tachoPulseCycle);
+  for(i = 0; i<=5; i++)
+  {
+    Serial.print(" A");
+    Serial.print(i);
+    Serial.print(analogReadVal[i]);
+  }
+  Serial.print("\n");
+}
+
