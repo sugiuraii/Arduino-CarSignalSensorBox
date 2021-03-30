ac63be0c6e28190622656146899099b2a34925d2
sz2 <sugiura.k.nigo@gmail.com>
2021-03-31 01:15:16 +0900

    Rename to ArduinoCarSignalSensor.

--- ArduinoCarSignalSensorBox/ArduinoCarSignalSensorBox.ino
+++ ArduinoCarSignalSensorBox/ArduinoCarSignalSensorBox.ino
@@ -30,7 +30,7 @@
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include "ArduinoTachoPulseReader.h"
+#include "ArduinoCarSignalSensorBox.h"
 #include "TachoSpeedPulseCounter.h"
 #include "SerialPortDump.h"
 #include "ADCRead.h"
--- ArduinoCarSignalSensorBox/TachoSpeedPulseCounter.h
+++ ArduinoCarSignalSensorBox/TachoSpeedPulseCounter.h
@@ -1,8 +1,6 @@
 #ifndef TACHO_SPEED_PULSE_COUNTER_H
 #define TACHO_SPEED_PULSE_COUNTER_H
 
-#include "ArduinoTachoPulseReader.h"
-
 void tachoSpeedPinSetup();
 inline unsigned long getSpeedPulseElapsedTime(const unsigned long nowTime);
 inline unsigned long getTachoPulseElapsedTime(const unsigned long nowTime);
